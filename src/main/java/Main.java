2f2ebf7421af03f37d29f160a0043520611cd50d
Kendra <kendra@Zipcoders-MacBook-Pro-45.local>
2022-07-02 10:37:59 -0400

    finished program

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,18 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter number");
+        int userInput = input.nextInt();
+        int sum = (userInput)*(userInput+1)/2;
+        System.out.println((sum));
+    }
 
     }
-}
+
